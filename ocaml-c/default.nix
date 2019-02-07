4884093621284f8a2edb83ad51d8e643383df301
rexim <reximkut@gmail.com>
2019-02-07 15:55:51 +0700

    Add NixOS environment for ocaml-c

---
+++ ocaml-c/default.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {}; {
+    cairoprobeEnv = stdenv.mkDerivation {
+        name = "cairoprobe-env";
+        buildInputs = [ stdenv
+                        gcc
+                        pkgconfig
+                        cairo
+                        SDL2
+                      ];
+    };
+}
